008efd34caddc0b79c08cda10044d5a206ed9c33
logantillman <logantillman@ymail.com>
2021-03-28 21:25:23 -0400

    WIP: gradescript progress 40/100

--- lab7/mymalloc.c
+++ lab7/mymalloc.c
@@ -35,6 +35,7 @@ void *my_malloc(size_t size) {
 
     void *listIt = free_list_begin();
     int isOpenMemory = 0;
+    int isHead = 0;
     struct chunk *openMemory = NULL;
 
     while (listIt != NULL) {
@@ -43,10 +44,19 @@ void *my_malloc(size_t size) {
         }
 
         struct chunk *tmpIt = (struct chunk *) listIt;
+        if (tmpIt->size >= size) {
+            // printf("Found chunk\n");
+            if (tmpIt == head) {
+                // printf("Found chunk is the head\n");
+                isHead = 1;
+            }
+            isOpenMemory = 1;
+            openMemory = tmpIt;
+        }
         listIt = free_list_next(listIt);
     }
 
-    if (head == NULL || size > head->size) {
+    if (head == NULL || isOpenMemory == 0) {
         if (size > 8192) {
             ptr = sbrk(size);
             allocPtr = (struct chunk *) ptr;
@@ -62,37 +72,74 @@ void *my_malloc(size_t size) {
             tmpHead->size = 8192 - size;
             // printf("allocPtr->size %d tmpHead->size %d\n", allocPtr->size, tmpHead->size);
             if (head == NULL) {
-//                printf("HEAD WAS NULL, SETTING TO ");
+                // printf("HEAD WAS NULL, SETTING TO ");
                 head = tmpHead;
                 head->next = NULL;
                 head->prev = NULL;
-//                printf("%08x size is %d\n", head, head->size);
+                // printf("%08x size is %d\n", head, head->size);
             }
             else {
-//                printf("Allocated memory, adding %d bytes to free list ", tmpHead->size);
-//                printf("old head %08x ", free_list_begin());
+                // printf("Allocated memory, adding %d bytes to free list ", tmpHead->size);
+                // printf("old head %08x ", free_list_begin());
                 head->prev = tmpHead;
                 tmpHead->next = head;
                 head = tmpHead;
-//                printf("new head %08x\n", free_list_begin());
+                // printf("new head %08x\n", free_list_begin());
             }
         }
     }
-    else {
+    else if (isOpenMemory == 1) {
         // printf("Head isn't null. Carve off the free list\n");
         // if (size <= head->size) {
-            allocPtr = head;
-            if (head->size == size) {
-                head = NULL;
+            // allocPtr = openMemory;
+            // printf("size: %d open: %08x alloc: %08x\n", size, openMemory, allocPtr);
+            if (openMemory->size == size) {
+                struct chunk *tmpChunk = NULL;
+                if (isHead == 1) {
+                    if (head->next != NULL) {
+                        tmpChunk = head->next;
+                    }
+                }
+
+                // printf("Carving %d bytes from mem chunk %08x of size %d\n", size, openMemory, openMemory->size);
+                if (openMemory->prev != NULL) {
+                    // printf("1: Linking %08x to %08x ", openMemory->prev, openMemory->next);
+                    openMemory->prev->next = openMemory->next;
+                }
+                if (openMemory->next != NULL) {
+                    // printf("2: Linking %08x to %08x\n", openMemory->next, openMemory->prev);
+                    openMemory->next->prev = openMemory->prev;
+                }
+                allocPtr = openMemory;
+
+                if (isHead == 1) {
+                    head = tmpChunk;
+                }
             }
             else {
-//                printf("Carving %d bytes off free list ", size);
+                // printf("Carving %d bytes off free list ", size);
+                struct chunk *tmpPrev = openMemory->prev;
+                struct chunk *tmpNext = openMemory->next;
 
-                head = (void *) head + size;
-                head->size = allocPtr->size - size;
-//                printf("Moved head from %08x to %08x\n", allocPtr, head);
+                allocPtr = openMemory;
+                openMemory = (void *) openMemory + size;
+                openMemory->size = allocPtr->size - size;
+                openMemory->prev = tmpPrev;
+                openMemory->next = tmpNext;
+                
+                if (openMemory->prev != NULL) {
+                    openMemory->prev->next = openMemory;
+                }
+                if (openMemory->next != NULL) {
+                    openMemory->next->prev = openMemory;
+                }
+
+                // printf("Moved open memory from %08x to %08x new size is %d\n", allocPtr, openMemory, openMemory->size);
+                if (isHead == 1) {
+                    head = openMemory;
+                    // printf("Set head to %08x\n", head);
+                }
             }
-            
             allocPtr->size = size;
             // printf("head addr: %08x head size: %d tmpAddr: %08x tmpSize: %d\n", head, head->size, allocPtr, allocPtr->size);
 
@@ -106,12 +153,20 @@ void *my_malloc(size_t size) {
     void *setWord = (void *) allocPtr + 4;
 
     strcpy((char *) setWord, "Til");
+    // printf("Made it to return allocPtr %08x size %d\n", allocPtr, allocPtr->size);
+
+    // struct chunk *listTrav = free_list_begin();
+    // while (listTrav != NULL) {
+    //     printf("%08x -> ", listTrav);
+    //     listTrav = free_list_next(listTrav);
+    // }
+    // printf("\n");
 
     return (void *) allocPtr + 8;
 }
 
 void my_free(void *ptr) {
-    // printf("called my free\n");
+    // printf("Current head: %08x Ptr: %08x\n", head, ptr);
 
     char *word = ptr - 4;
 
@@ -130,24 +185,25 @@ void my_free(void *ptr) {
         head->prev = NULL;
     }
     else {
-//        printf("Adding free'd chunk of size %d to head of free list ", tmpPtr->size);
-//        printf("old head %08x ", free_list_begin());
-        struct chunk *tmp = (struct chunk *) ptr;
-        head->prev = tmp;
-        tmp->next = head;
-        head = tmp;
-//        printf("new head %08x\n", free_list_begin());
+        // printf("Adding free'd chunk of size %d to head of free list ", tmpPtr->size);
+        // printf("old head %08x tmpPtr %08x ", head, tmpPtr);
+        // struct chunk *tmp = (struct chunk *) ptr;
+        head->prev = tmpPtr;
+        tmpPtr->next = head;
+        tmpPtr->prev = NULL;
+        head = tmpPtr;
+        // printf("new head %08x next %08x\n", free_list_begin(), free_list_next(free_list_begin()));
         // printf("next %08x\n", free_list_next(free_list_begin()));
     }
 
     /* Printing out the free list */
-//    printf("    Free List:\n");
-    void *it = free_list_begin();
-    while (it != NULL) {
-        struct chunk *fin = (struct chunk *) it;
-//        printf("    %08x it->size %d\n", it, fin->size);
-        it = free_list_next(it);
-    }
+    // printf("    Free List:\n");
+    // void *it = free_list_begin();
+    // while (it != NULL) {
+    //     struct chunk *fin = (struct chunk *) it;
+    //     printf("    %08x it->size %d\n", it, fin->size);
+    //     it = free_list_next(it);
+    // }
 }
 
 void *free_list_begin() {
