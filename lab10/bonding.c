24039602230696603cef3210cb6257c5e1846d5d
logantillman <logantillman@ymail.com>
2021-04-23 18:22:34 -0400

    feat: Initial commit

---
+++ lab10/bonding.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include "bonding.h"
+
+/* This solution compiles and hangs. */
+
+void *initialize_v(char *verbosity)
+{
+  return NULL;
+}
+
+void *hydrogen(void *arg)
+{
+  return NULL;
+}
+
+void *oxygen(void *arg)
+{
+  return NULL;
+}
