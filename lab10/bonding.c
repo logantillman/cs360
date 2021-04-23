191db9866cbf4f499f77f642f544cbef69e7964d
logantillman <logantillman@ymail.com>
2021-04-23 19:52:35 -0400

    feat: Add structs

--- lab10/bonding.c
+++ lab10/bonding.c
@@ -1,23 +1,35 @@
+// Author: Logan Tillman
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
 #include "bonding.h"
+#include "dllist.h"
 
-/* This solution compiles and hangs. */
+typedef struct {
+    pthread_mutex_t *lock;
+    Dllist waitHydrogen;
+    Dllist waitOxygen;
+} GlobalInfo;
 
-void *initialize_v(char *verbosity)
-{
+typedef struct {
+    pthread_cond_t *cond;
+    pthread_t *tid;
+    pthread_t *mol1;
+    pthread_t *mol2;
+    pthread_t *mol3;
+} Molecule;
+
+void *initialize_v(char *verbosity) {
   return NULL;
 }
 
-void *hydrogen(void *arg)
-{
+void *hydrogen(void *arg) {
   return NULL;
 }
 
-void *oxygen(void *arg)
-{
+void *oxygen(void *arg) {
   return NULL;
 }
